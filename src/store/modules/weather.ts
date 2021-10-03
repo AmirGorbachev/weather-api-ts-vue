181cb946e72d6c4cc039c2121ff3e8375f5f495b
Amir Gorebachev <32037802+AmirGorbachev@users.noreply.github.com>
2021-10-03 16:44:11 +0700

    Init store module

---
+++ src/store/modules/weather.ts
@@ -0,0 +1,18 @@
+// store/modules/user.ts
+import { VuexModule, Module, Mutation, Action } from 'vuex-module-decorators'
+
+@Module({ namespaced: true, name: 'test' })
+
+class Weather extends VuexModule {
+  public name: string = ''
+  @Mutation
+  public setName(newName: string): void {
+    this.name = newName
+  }
+  @Action
+  public updateName(newName: string): void {
+    this.context.commit('setName', newName)
+  }
+}
+
+export default Weather
